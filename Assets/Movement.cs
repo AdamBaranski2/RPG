aade5b81519546f80ea223890057c598afe4641a
DESKTOP-GPU541K\Florian <chomik126@gmail.com>
2021-02-01 16:46:17 +0100

    no message

---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("Sprint", true);
+        }
+        else
+        {
+            animator.SetBool("Sprint", false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha1)) UseSkill(1);
+
+        if (Input.GetKeyUp(KeyCode.Alpha2)) UseSkill(2);
+
+        if (Input.GetKeyUp(KeyCode.Alpha3)) UseSkill(3);
+
+        if (Input.GetKeyUp(KeyCode.Alpha4)) UseSkill(4);
+    }
+    void UseSkill(int skillNumber)
+    {
+        animator.SetTrigger("Skill" + skillNumber);
+
+    }
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+
+}
